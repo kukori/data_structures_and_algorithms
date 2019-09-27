99b14c23d7a8b73c73a992b2d2aa4522995342aa
attilav <attila.varjas@gmail.com>
2019-09-27 09:24:10 +0700

    insertion sort comment fix

--- algorithms_insertion_sort.js
+++ algorithms_insertion_sort.js
@@ -1,6 +1,6 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-// best case O(n) when nearly sorted, worst case O(n^n)
+// best case O(n) when nearly sorted, worst case O(n^2)
 function insertionSort(array) {
     for(let i = 1; i < array.length; i++){
         let temp = array[i];

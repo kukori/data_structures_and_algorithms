949d7fcf469edbe2dfaf6398a14ad0923e468126
attilav <attila.varjas@gmail.com>
2019-09-27 09:20:05 +0700

    insertion sort

---
+++ algorithms_insertion_sort.js
@@ -0,0 +1,17 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++){
+        let temp = array[i];
+        for(let j = 0; j < array.length; j++) {
+            if(temp < array[j]) {
+                array.splice(i, 1);
+                array.splice(j, 0, temp);
+                break;
+            }
+        }
+    }
+}
+
+insertionSort(numbers);
+console.log(numbers);

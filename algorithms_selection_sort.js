ce93b47d4de4eaf45839946f6cface457af1b1f8
attilav <attila.varjas@gmail.com>
2019-09-27 09:25:58 +0700

    selection sort comment

--- algorithms_selection_sort.js
+++ algorithms_selection_sort.js
@@ -1,6 +1,6 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-// O(n^2)
+// O(n^2) 
 function selectionSort(array) {
     for(let i = 0; i < array.length; i++){
         let smallestItemIndex = i;

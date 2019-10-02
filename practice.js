81abd565680dc9130b99b06f9f693c2fb5f6f1f0
attilav <attila.varjas@gmail.com>
2019-10-02 09:20:31 +0700

    inventory update

--- practice.js
+++ practice.js
@@ -44,5 +44,53 @@ function sym() {
     return symDiff(array1, args);
 }
 
-console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
+//console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
 
+function updateInventory(arr1, arr2) {
+    const temp = {};
+    const result = [];
+    for(let i = 0; i < arr1.length; i++) {
+        if(!temp[arr1[i][1]]) {
+            temp[arr1[i][1]] = arr1[i][0];
+        } else {
+            temp[arr1[i][1]] += arr1[i][0];
+        }
+    }
+
+    for(let j = 0; j < arr2.length; j++) {
+        if(!temp[arr2[j][1]]) {
+            temp[arr2[j][1]] = arr2[j][0];
+        } else {
+            temp[arr2[j][1]] += arr2[j][0];
+        }
+    }
+
+    const keys = Object.keys(temp);
+    //insertion sort
+    keys.sort();
+    console.log(keys);
+
+    for(let k = 0; k < keys.length; k++) {
+        result.push([temp[keys[k]], keys[k]]);
+    }
+    
+    return result;
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+//console.log(updateInventory(curInv, newInv));
+//console.log(updateInventory([], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]));
